a3ce9a3033ac1f7649dbe98f42a38d2752b1e0ef
Mahe-Thomas <thomas.mahe@hamlab.fr>
2020-02-05 11:54:25 +0100

    Basic scenario 1
    fixup

--- main.py
+++ main.py
@@ -6,3 +6,5 @@ if __name__ == '__main__':
 
     classes = []
     meta_model = tx.metamodel_from_file('grammar.tx', classes=classes, builtins=type_builtins)
+
+    model = meta_model.model_from_file('samples/basic_scenario_1.rml')
