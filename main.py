4d5e30dcbabd041b3683a44b0b60186e3bca2a18
Aymeric Valdenaire <aymericvaldenaire@hotmail.fr>
2020-02-12 10:23:22 +0100

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class Beat(object):
     def __init__(self,name):
         self.name = name
 
+    model = meta_model.model_from_file('samples/old_basic_scenario_1.rml')
 
 class Note(object):
     def __init__(self, value, tick_pos, duration):
