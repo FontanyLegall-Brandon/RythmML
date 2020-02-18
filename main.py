323ba7b964324c5acb49261a4947250a248daf58
Aymeric Valdenaire <aymericvaldenaire@hotmail.fr>
2020-02-18 14:31:58 +0100

    Model

--- main.py
+++ main.py
@@ -1,23 +1,57 @@
-import textx as tx
-import time
-import rtmidi
+class Model(object):
+    def __init__(self, parent, name, bpm, bar_list, section_list, track):
+        self.parent = parent
+        self.name = name
+        self.bpm = bpm
+        self.bar_list = bar_list
+        self.section_list = section_list
+        self.track = track
+
+
+class Section(object):
+    def __init__(self, parent, name, bar_list):
+        self.parent = parent
+        self.name = name
+        self.bar_list = bar_list
+
 
 class Track(object):
-    def __init__(self,name):
+    def __init__(self, parent, name, section_list):
+        self.parent = parent
         self.name = name
+        self.section_list = section_list
+
 
 class Bar(object):
-    def __init__(self,name):
+    def __init__(self, parent, name, pattern, note_lines):
+        self.parent = parent
         self.name = name
+        self.pattern = pattern
+        self.note_lines = note_lines
 
-class Beat(object):
-    def __init__(self,name):
+
+class Pattern(object):
+    def __init__(self, parent, name, list):
+        self.parent = parent
         self.name = name
+        self.list = list  # liste d'entiers
 
-    model = meta_model.model_from_file('samples/old_basic_scenario_1.rml')
+
+class NotePattern(object):
+    def __init__(self, parent, name, value):
+        self.parent = parent
+        self.name = name
+        self.value = value  # string
+
+
+class NoteList(object):
+    def __init__(self, parent, note_pattern, note):
+        self.parent = parent
+        self.note_pattern = note_pattern
+        self.note = note
+
 
 class Note(object):
-    def __init__(self, value, tick_pos, duration):
+    def __init__(self, parent, value):
+        self.parent = parent
         self.value = value
-        self.tick_pos = tick_pos
-        self.duration = duration
