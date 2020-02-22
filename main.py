6ad093ed7ea7c1c9235156366e72fc275c87210a
Mahe-Thomas <thomas.mahe@hamlab.fr>
2020-02-22 09:08:26 +0100

    mido setup

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 import textx as tx
-import mido 
+import mido
+from mido import tick2second, bpm2tempo
 
-NOTE = {'bd': 60}
-
+NOTE = {'drum' : {'bd' : 35, 'sd' : 38, 'rc': 51}}
 
 class Model(object):
-    def __init__(self, sections, tracks, bpm, patterns):
+    def __init__(self, sections, track, bpm, patterns):
         self.sections = sections
-        self.tracks = tracks
+        self.track = track
         self.bpm = bpm
         self.patterns = patterns
 
@@ -21,8 +21,40 @@ class Model(object):
         out += str(self.bpm) + '\n\n'
         out += '\n'.join([str(pattern) for pattern in self.patterns])
         out += '\n'.join([str(section) for section in self.sections])
-        out += '\n'.join([str(track) for track in self.tracks])
+        out += str(self.track)
         return out
+
+
+    def __repr__(self):
+        
+        mid = mido.MidiFile(type=2)
+        sections_config = [e.get_notes() for e in self.track.sections_config]
+        instruments_set = list()
+
+        for section_config in sections_config:
+            for value in section_config.values():
+                if type(value) is list :
+                    instruments_set += value
+        instruments_set = set(list(map(lambda e : e.instrument, instruments_set)))
+
+        midi_tracks = dict()
+        for instrument in instruments_set : 
+            midi_tracks[instrument] = mido.MidiTrack()
+
+        for section_config in sections_config:
+            for key in section_config.keys():
+                if type(key) is int :
+                    for note in section_config[key] :
+                        midi_tracks[note.instrument].append(mido.Message('program_change', program=10, time=0))
+                        midi_tracks[note.instrument].append(mido.Message('note_on',note=NOTE[note.instrument][note.note],time=int(tick2second(1, ticks_per_beat=4, tempo=bpm2tempo(self.bpm))*1000)))
+                        midi_tracks[note.instrument].append(mido.Message('note_off',note=NOTE[note.instrument][note.note],time=int(tick2second(2, ticks_per_beat=4, tempo=bpm2tempo(self.bpm))*1000)))
+            print(section_config)
+
+        for track in midi_tracks.values():
+            mid.tracks.append(track)
+        
+        mid.save("test.mid")
+        return str(instruments_set)
 
 
 class Pattern(object):
@@ -62,7 +94,6 @@ class Section:
     
     def validate(self):
         size_tab = self.pattern.get_beats_size()
-        print(size_tab)
         for bar in self.bars :
             if(bar.get_beats_size() != size_tab):
                 raise SyntaxError("Bar {} mismatch with pattern {} = {}".format(repr(bar),
@@ -81,10 +112,58 @@ class SectionConfig:
         self.repeatCount = repeatCount
         self.section = section
         
+    def get_bars(self):
+        return self.section.bars
+
+
+    def get_notes(self):
+        out = dict()
+        out['startTime'] = self.startTime
+        if self.repeatCount > 0 :
+            for bar in self.get_bars():
+                tick_offset = 0
+                
+                for i in range(self.repeatCount) :
+                    for beats in bar.ticks[0].split('|'):
+                        for tick in beats :
+                            if tick in ['x'] :
+                                if tick_offset not in out :
+                                    out[tick_offset] = [bar.note]
+                                else :
+                                    out[tick_offset].append(bar.note)
+                            tick_offset += 1
+        else :
+            for bar in self.get_bars():
+                tick_offset = 0
+                for beats in bar.ticks[0].split('|'):
+                    for tick in beats :
+                        if tick in ['x'] :
+                            if tick_offset not in out :
+                                out[tick_offset] = [bar.note]
+                            else :
+                                out[tick_offset].append(bar.note)
+                        tick_offset += 1
+        return out
+
+    def __repr__(self):
+        return str(self.get_notes())
+
     def __str__(self):
         if self.startTime < 0 :
             raise SyntaxError("Start time can't be lower than 0")
         return "section modifier : "+ str(self.startTime)+ " : "+ str(self.repeatCount) + " section "+ str(self.section)
+
+class Note:
+    def __init__(self, parent, instrument, note):
+        self.parent = parent
+        self.instrument = instrument
+        self.note = note
+
+    def __repr__(self):
+        return '<Note {} {}>'.format(self.instrument, self.note)
+
+    def __str__(self):
+        return '<Note {} {}>'.format(self.instrument, self.note)
 
 class Bar(object):
 
@@ -102,12 +181,12 @@ class Bar(object):
     
     def __str__(self):
         # TODO put midi code inside stp
-        return '\n'.join([tick for tick in self.ticks]) + ' ' + self.note
+        return '\n'.join([tick for tick in self.ticks]) + ' ' + repr(self.note)
 
 
 if __name__ == '__main__':
 
-    classes = [Model, Bar,SectionConfig,  Track, Pattern, Section]
+    classes = [Model, Bar,SectionConfig,  Track, Pattern, Section, Note]
 
     meta_model = tx.metamodel_from_file('grammar.tx', classes=classes)
     for file_name in os.listdir("./samples"):
@@ -119,4 +198,6 @@ if __name__ == '__main__':
         out = open('out/{}'.format(file_name.replace('.rml', '.py')), 'w')
         print(model, file=out)
         print(model)
+        print("REPR OF MODEL")
+        print(repr(model))
         out.close()
